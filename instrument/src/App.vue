60c7ee12c51e0a2d49f0ba643a883cccb02b1311
Seyoung Hong <maiorem00@gmail.com>
2022-07-19 13:42:04 +0900

    img assets

--- instrument/src/App.vue
+++ instrument/src/App.vue
@@ -17,8 +17,10 @@ export default {
 </script>
 
 <style>
+@import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
